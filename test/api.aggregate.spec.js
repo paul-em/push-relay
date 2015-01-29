def649ef3b0cd19626b7f9b003a807a1aab4a577
_paulem <paul3m@gmail.com>
2015-01-29 22:32:12 +0100

    added put and aggregate route and tests

--- test/api.aggregate.spec.js
+++ test/api.aggregate.spec.js
@@ -1,9 +1,106 @@
 'use strict';
-
 var expect = require("expect.js");
+var request = require('request');
+var nock = require('nock');
+var helper = require('./helper');
+helper.setProcessArgs('--cert ./test/ssl/test.crt --key ./test/ssl/test.key --apikey ./test/apikey/apikey.txt');
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; // Avoids DEPTH_ZERO_SELF_SIGNED_CERT error for self-signed certs
 
-describe('api aggregate', function(){
-    it('should pass test', function(){
-        expect(1).to.eql(1);
+
+nock('https://android.googleapis.com:443')
+    .post('/gcm/send', function (body) {
+        return body.data.data === 'right aggregated data';
+    })
+    .reply(200, {
+        success: 2,
+        failure: 0,
+        multicast_id: 5201523458607294000,
+        canonical_ids: 0,
+        results: [
+            {
+                message_id: 242344935,
+                registration_id: 23942395439,
+                error: null
+            },
+            {
+                message_id: 242344936,
+                registration_id: 23942395439,
+                error: null
+            }
+        ]
+    });
+
+nock('https://android.googleapis.com:443')
+    .post('/gcm/send', function (body) {
+        return body.data.data === 'wrong aggregated data';
+    })
+    .reply(200, {
+        multicast_id: 5201523458607294000,
+        success: 0,
+        failure: 2,
+        canonical_ids: 0,
+        results: [
+            {
+                error: 'InvalidRegistration'
+            },
+            {
+                error: 'InvalidRegistration'
+            }
+        ]
+    });
+
+
+describe('api aggregate', function () {
+    before(function(){
+        require('./../lib');
+    });
+    it('should successfully send push message', function (done) {
+        request({
+            method: 'PUT',
+            url: 'https://localhost:443/a',
+            json: true,
+            body: {
+                data:'right aggregated data',
+                subscriptionIds:
+                    [
+                        'APA91bFNE4p2LmDNaZhuoBV-W6fRlE_nYnGjLcEZa3C79OU',
+                        'APA91bFNE4p2LmDNaZhuoBV-W6fRlE_nYnGjLcEZa3C79OI'
+                    ]
+            }
+        }, function (error, response, body) {
+            expect(response.statusCode).to.be(200);
+            done();
+        });
+    });
+
+    it('should return a 400 error for wrong json format', function (done) {
+        request({
+            method: 'PUT',
+            url: 'https://localhost:443/a',
+            json: true,
+            body: 'nojson'
+        }, function (error, response, body) {
+            expect(response.statusCode).to.be(400);
+            done();
+        });
+    });
+
+    it('should return a 401 auth error for wrong id', function (done) {
+        request({
+            method: 'PUT',
+            url: 'https://localhost:443/a',
+            json: true,
+            body: {
+                data:'wrong aggregated data',
+                subscriptionIds:
+                    [
+                        'APA91bFNE4p2LmDNaZhuoBV-W6fRlE_nYnGjLcEZa3C79OU',
+                        'APA91bFNE4p2LmDNaZhuoBV-W6fRlE_nYnGjLcEZa3C79OI'
+                    ]
+            }
+        }, function (error, response, body) {
+            expect(response.statusCode).to.be(401);
+            done();
+        });
     });
 });
--- test/start.spec.js
+++ test/start.spec.js
@@ -1,48 +1,50 @@
 'use strict';
 var expect = require('expect.js');
 var helper = require('./helper');
+var proxyquire =  require('proxyquire');
 
 function start(){
-    require('./../lib');
+    proxyquire('./../lib', {});
 }
 
 describe('server start', function(){
     it('should throw a missing arguments error', function(){
+        helper.setProcessArgs('--port 8000');
         expect(start).to.throwException(/arguments/);
     });
 
     it('should throw missing cert error', function(){
-        helper.setProcessArgs('--key ./test/ssl/test.key --apikey ./test/apikey/apikey.txt');
+        helper.setProcessArgs('--key ./test/ssl/test.key --apikey ./test/apikey/apikey.txt --port 8001');
         expect(start).to.throwException(/arguments/);
     });
 
     it('should throw missing key error', function(){
-        helper.setProcessArgs('--cert ./test/ssl/test.crt --apikey ./test/apikey/apikey.txt');
+        helper.setProcessArgs('--cert ./test/ssl/test.crt --apikey ./test/apikey/apikey.txt --port 8002');
         expect(start).to.throwException(/arguments/);
     });
 
     it('should throw missing apikey error', function(){
-        helper.setProcessArgs('--cert ./test/ssl/test.crt --key ./test/ssl/test.key');
+        helper.setProcessArgs('--cert ./test/ssl/test.crt --key ./test/ssl/test.key --port 8003');
         expect(start).to.throwException(/arguments/);
     });
 
     it('should throw wrong cert error', function(){
-        helper.setProcessArgs('--cert ./test/ssl/wrongtest.crt --key ./test/ssl/test.key --apikey ./test/apikey/apikey.txt');
+        helper.setProcessArgs('--cert ./test/ssl/wrongtest.crt --key ./test/ssl/test.key --apikey ./test/apikey/apikey.txt --port 8004');
         expect(start).to.throwException(/no such file/);
     });
 
     it('should throw wrong key error', function(){
-        helper.setProcessArgs('--cert ./test/ssl/test.crt --key ./test/ssl/wrongtest.key --apikey ./test/apikey/apikey.txt');
+        helper.setProcessArgs('--cert ./test/ssl/test.crt --key ./test/ssl/wrongtest.key --apikey ./test/apikey/apikey.txt --port 8005');
         expect(start).to.throwException(/no such file/);
     });
 
     it('should throw wrong apikey error', function(){
-        helper.setProcessArgs('--cert ./test/ssl/test.crt --key ./test/ssl/test.key --apikey wrong');
+        helper.setProcessArgs('--cert ./test/ssl/test.crt --key ./test/ssl/test.key --apikey wrong --port 8006');
         expect(start).to.throwException(/no such file/);
     });
 
     it('should successfully start server', function(){
-        helper.setProcessArgs('--cert ./test/ssl/test.crt --key ./test/ssl/test.key --apikey ./test/apikey/apikey.txt');
+        helper.setProcessArgs('--cert ./test/ssl/test.crt --key ./test/ssl/test.key --apikey ./test/apikey/apikey.txt --port 8007');
         expect(start).to.not.throwException();
     });
 });
